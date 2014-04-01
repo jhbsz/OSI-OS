b15a3c3ef5e9d25d333402ad99253cff3c0bb760
Justin Muniz <justin.muniz@maine.edu>
2014-04-01 06:34:11 -0400

    Update gen.c
    Stripped comments, changed formatting

--- Operating-System/Executable-Commands/Core-Commands/User-Utilities/Copy-and-Convert/gen.c
+++ Operating-System/Executable-Commands/Core-Commands/User-Utilities/Copy-and-Convert/gen.c
@@ -1,14 +1,6 @@
-/*-
- * This program is in the public domain
- *
- * $FreeBSD$
- */
-
 #include <stdio.h>
 
-int
-main(int argc __unused, char **argv __unused)
-{
+int main(int argc __unused, char **argv __unused) {
 	int i;
 
 	for (i = 0; i < 256; i++)
