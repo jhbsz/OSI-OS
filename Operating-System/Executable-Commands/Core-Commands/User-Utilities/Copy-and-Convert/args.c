c031c42878d4131b8f58cdf81a82b21715ac8235
Justin Muniz <justin.muniz@maine.edu>
2014-04-01 06:21:57 -0400

    Update args.c
    Stripped comments and FBSDID and lint information

--- Operating-System/Executable-Commands/Core-Commands/User-Utilities/Copy-and-Convert/args.c
+++ Operating-System/Executable-Commands/Core-Commands/User-Utilities/Copy-and-Convert/args.c
@@ -1,46 +1,5 @@
-/*-
- * Copyright (c) 1991, 1993, 1994
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Keith Muller of the University of California, San Diego and Lance
- * Visser of Convex Computer Corporation.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-#ifndef lint
-#if 0
-static char sccsid[] = "@(#)args.c	8.3 (Berkeley) 4/2/94";
-#endif
-#endif /* not lint */
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
-
 #include <sys/types.h>
-
 #include <err.h>
 #include <errno.h>
 #include <inttypes.h>
@@ -48,12 +7,12 @@ __FBSDID("$FreeBSD$");
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "dd.h"
 #include "extern.h"
 
 static int	c_arg(const void *, const void *);
 static int	c_conv(const void *, const void *);
+static char *oper;
 static void	f_bs(char *);
 static void	f_cbs(char *);
 static void	f_conv(char *);
@@ -90,19 +49,11 @@ static const struct arg {
 	{ "skip",	f_skip,		C_SKIP,	 C_SKIP },
 };
 
-static char *oper;
-
-/*
- * args -- parse JCL syntax of dd.
- */
-void
-jcl(char **argv)
-{
+void jcl(char **argv) {
 	struct arg *ap, tmp;
 	char *arg;
 
 	in.dbsz = out.dbsz = 512;
-
 	while ((oper = *++argv) != NULL) {
 		if ((oper = strdup(oper)) == NULL)
 			errx(1, "unable to allocate space for the argument \"%s\"", *argv);
@@ -113,37 +64,19 @@ jcl(char **argv)
 			errx(1, "no value specified for %s", oper);
 		tmp.name = oper;
 		if (!(ap = (struct arg *)bsearch(&tmp, args,
-		    sizeof(args)/sizeof(struct arg), sizeof(struct arg),
-		    c_arg)))
+		    sizeof(args)/sizeof(struct arg), sizeof(struct arg), c_arg)))
 			errx(1, "unknown operand %s", tmp.name);
 		if (ddflags & ap->noset)
-			errx(1, "%s: illegal argument combination or already set",
-			    tmp.name);
+			errx(1, "%s: illegal argument combination or already set", tmp.name);
 		ddflags |= ap->set;
 		ap->f(arg);
 	}
-
-	/* Final sanity checks. */
-
 	if (ddflags & C_BS) {
-		/*
-		 * Bs is turned off by any conversion -- we assume the user
-		 * just wanted to set both the input and output block sizes
-		 * and didn't want the bs semantics, so we don't warn.
-		 */
-		if (ddflags & (C_BLOCK | C_LCASE | C_SWAB | C_UCASE |
-		    C_UNBLOCK))
+		if (ddflags & (C_BLOCK | C_LCASE | C_SWAB | C_UCASE | C_UNBLOCK))
 			ddflags &= ~C_BS;
-
-		/* Bs supersedes ibs and obs. */
 		if (ddflags & C_BS && ddflags & (C_IBS | C_OBS))
 			warnx("bs supersedes ibs and obs");
 	}
-
-	/*
-	 * Ascii/ebcdic and cbs implies block/unblock.
-	 * Block/unblock requires cbs and vice-versa.
-	 */
 	if (ddflags & (C_BLOCK | C_UNBLOCK)) {
 		if (!(ddflags & C_CBS))
 			errx(1, "record operations require cbs");
@@ -163,27 +96,15 @@ jcl(char **argv)
 			errx(1, "cbs meaningless if not doing record operations");
 	} else
 		cfunc = def;
-
-	/*
-	 * Bail out if the calculation of a file offset would overflow.
-	 */
-	if (in.offset > OFF_MAX / (ssize_t)in.dbsz ||
-	    out.offset > OFF_MAX / (ssize_t)out.dbsz)
-		errx(1, "seek offsets cannot be larger than %jd",
-		    (intmax_t)OFF_MAX);
+	if (in.offset > OFF_MAX / (ssize_t)in.dbsz || out.offset > OFF_MAX / (ssize_t)out.dbsz)
+		errx(1, "seek offsets cannot be larger than %jd", (intmax_t)OFF_MAX);
 }
 
-static int
-c_arg(const void *a, const void *b)
-{
-
-	return (strcmp(((const struct arg *)a)->name,
-	    ((const struct arg *)b)->name));
+static int c_arg(const void *a, const void *b) {
+	return (strcmp(((const struct arg *)a)->name, ((const struct arg *)b)->name));
 }
 
-static void
-f_bs(char *arg)
-{
+static void f_bs(char *arg) {
 	uintmax_t res;
 
 	res = get_num(arg);
@@ -192,9 +113,7 @@ f_bs(char *arg)
 	in.dbsz = out.dbsz = (size_t)res;
 }
 
-static void
-f_cbs(char *arg)
-{
+static void f_cbs(char *arg) {
 	uintmax_t res;
 
 	res = get_num(arg);
@@ -203,9 +122,7 @@ f_cbs(char *arg)
 	cbsz = (size_t)res;
 }
 
-static void
-f_count(char *arg)
-{
+static void f_count(char *arg) {
 	intmax_t res;
 
 	res = (intmax_t)get_num(arg);
@@ -217,78 +134,53 @@ f_count(char *arg)
 		cpy_cnt = (uintmax_t)res;
 }
 
-static void
-f_files(char *arg)
-{
-
+static void f_files(char *arg) {
 	files_cnt = get_num(arg);
 	if (files_cnt < 1)
 		errx(1, "files must be between 1 and %jd", (uintmax_t)-1);
 }
 
-static void
-f_fillchar(char *arg)
-{
-
+static void f_fillchar(char *arg) {
 	if (strlen(arg) != 1)
 		errx(1, "need exactly one fill char");
-
 	fill_char = arg[0];
 }
 
-static void
-f_ibs(char *arg)
-{
+static void f_ibs(char *arg) {
 	uintmax_t res;
-
+	
 	if (!(ddflags & C_BS)) {
 		res = get_num(arg);
 		if (res < 1 || res > SSIZE_MAX)
-			errx(1, "ibs must be between 1 and %jd",
-			    (intmax_t)SSIZE_MAX);
+			errx(1, "ibs must be between 1 and %jd", (intmax_t)SSIZE_MAX);
 		in.dbsz = (size_t)res;
 	}
 }
 
-static void
-f_if(char *arg)
-{
-
+static void f_if(char *arg) {
 	in.name = arg;
 }
 
-static void
-f_obs(char *arg)
-{
+static void f_obs(char *arg) {
 	uintmax_t res;
-
+	
 	if (!(ddflags & C_BS)) {
 		res = get_num(arg);
 		if (res < 1 || res > SSIZE_MAX)
-			errx(1, "obs must be between 1 and %jd",
-			    (intmax_t)SSIZE_MAX);
+			errx(1, "obs must be between 1 and %jd", (intmax_t)SSIZE_MAX);
 		out.dbsz = (size_t)res;
 	}
 }
 
-static void
-f_of(char *arg)
-{
-
+static void f_of(char *arg) {
 	out.name = arg;
 }
 
-static void
-f_seek(char *arg)
-{
-
+static void f_seek(char *arg) {
 	out.offset = get_off_t(arg);
 }
 
-static void
-f_skip(char *arg)
-{
-
+static void f_skip(char *arg) {
 	in.offset = get_off_t(arg);
 }
 
@@ -319,15 +211,12 @@ static const struct conv {
 	{ "unblock",	C_UNBLOCK,	C_BLOCK,	NULL },
 };
 
-static void
-f_conv(char *arg)
-{
+static void f_conv(char *arg) {
 	struct conv *cp, tmp;
 
 	while (arg != NULL) {
 		tmp.name = strsep(&arg, ",");
-		cp = bsearch(&tmp, clist, sizeof(clist) / sizeof(struct conv),
-		    sizeof(struct conv), c_conv);
+		cp = bsearch(&tmp, clist, sizeof(clist) / sizeof(struct conv), sizeof(struct conv), c_conv);
 		if (cp == NULL)
 			errx(1, "unknown conversion %s", tmp.name);
 		if (ddflags & cp->noset)
@@ -338,79 +227,56 @@ f_conv(char *arg)
 	}
 }
 
-static int
-c_conv(const void *a, const void *b)
-{
-
-	return (strcmp(((const struct conv *)a)->name,
-	    ((const struct conv *)b)->name));
+static int c_conv(const void *a, const void *b) {
+	return (strcmp(((const struct conv *)a)->name, ((const struct conv *)b)->name));
 }
 
-/*
- * Convert an expression of the following forms to a uintmax_t.
- * 	1) A positive decimal number.
- *	2) A positive decimal number followed by a 'b' or 'B' (mult by 512).
- *	3) A positive decimal number followed by a 'k' or 'K' (mult by 1 << 10).
- *	4) A positive decimal number followed by a 'm' or 'M' (mult by 1 << 20).
- *	5) A positive decimal number followed by a 'g' or 'G' (mult by 1 << 30).
- *	5) A positive decimal number followed by a 'w' or 'W' (mult by sizeof int).
- *	6) Two or more positive decimal numbers (with/without [BbKkMmGgWw])
- *	   separated by 'x' or 'X' (also '*' for backwards compatibility),
- *	   specifying the product of the indicated values.
- */
-static uintmax_t
-get_num(const char *val)
-{
+static uintmax_t get_num(const char *val) {
 	uintmax_t num, mult, prevnum;
 	char *expr;
 
 	errno = 0;
 	num = strtouq(val, &expr, 0);
-	if (errno != 0)				/* Overflow or underflow. */
+	if (errno != 0)
 		err(1, "%s", oper);
-	
-	if (expr == val)			/* No valid digits. */
+	if (expr == val)
 		errx(1, "%s: illegal numeric value", oper);
-
 	mult = 0;
 	switch (*expr) {
-	case 'B':
-	case 'b':
-		mult = 512;
-		break;
-	case 'K':
-	case 'k':
-		mult = 1 << 10;
-		break;
-	case 'M':
-	case 'm':
-		mult = 1 << 20;
-		break;
-	case 'G':
-	case 'g':
-		mult = 1 << 30;
-		break;
-	case 'W':
-	case 'w':
-		mult = sizeof(int);
-		break;
-	default:
-		;
+		case 'B':
+		case 'b':
+			mult = 512;
+			break;
+		case 'K':
+		case 'k':
+			mult = 1 << 10;
+			break;
+		case 'M':
+		case 'm':
+			mult = 1 << 20;
+			break;
+		case 'G':
+		case 'g':
+			mult = 1 << 30;
+			break;
+		case 'W':
+		case 'w':
+			mult = sizeof(int);
+			break;
+		default:
+			;
 	}
-
 	if (mult != 0) {
 		prevnum = num;
 		num *= mult;
-		/* Check for overflow. */
 		if (num / mult != prevnum)
 			goto erange;
 		expr++;
 	}
-
 	switch (*expr) {
 		case '\0':
 			break;
-		case '*':			/* Backward compatible. */
+		case '*':
 		case 'X':
 		case 'x':
 			mult = get_num(expr + 1);
@@ -426,63 +292,50 @@ erange:
 	return (num);
 }
 
-/*
- * Convert an expression of the following forms to an off_t.  This is the
- * same as get_num(), but it uses signed numbers.
- *
- * The major problem here is that an off_t may not necessarily be a intmax_t.
- */
-static off_t
-get_off_t(const char *val)
-{
+static off_t get_off_t(const char *val) {
 	intmax_t num, mult, prevnum;
 	char *expr;
 
 	errno = 0;
 	num = strtoq(val, &expr, 0);
-	if (errno != 0)				/* Overflow or underflow. */
+	if (errno != 0)
 		err(1, "%s", oper);
-	
-	if (expr == val)			/* No valid digits. */
+	if (expr == val)
 		errx(1, "%s: illegal numeric value", oper);
-
 	mult = 0;
 	switch (*expr) {
-	case 'B':
-	case 'b':
-		mult = 512;
-		break;
-	case 'K':
-	case 'k':
-		mult = 1 << 10;
-		break;
-	case 'M':
-	case 'm':
-		mult = 1 << 20;
-		break;
-	case 'G':
-	case 'g':
-		mult = 1 << 30;
-		break;
-	case 'W':
-	case 'w':
-		mult = sizeof(int);
-		break;
-	}
-
+		case 'B':
+		case 'b':
+			mult = 512;
+			break;
+		case 'K':
+		case 'k':
+			mult = 1 << 10;
+			break;
+		case 'M':
+		case 'm':
+			mult = 1 << 20;
+			break;
+		case 'G':
+		case 'g':
+			mult = 1 << 30;
+			break;
+		case 'W':
+		case 'w':
+			mult = sizeof(int);
+			break;
+		}
 	if (mult != 0) {
 		prevnum = num;
 		num *= mult;
-		/* Check for overflow. */
 		if ((prevnum > 0) != (num > 0) || num / mult != prevnum)
 			goto erange;
 		expr++;
 	}
-
 	switch (*expr) {
 		case '\0':
 			break;
-		case '*':			/* Backward compatible. */
+		case '*':
 		case 'X':
 		case 'x':
 			mult = (intmax_t)get_off_t(expr + 1);
