9fc969800b16c88dde4ef05e8e6f739f35559457
Justin Muniz <justin.muniz@maine.edu>
2014-04-01 06:39:43 -0400

    Update dd.h
    Stripped comments and changed formatting

--- Operating-System/Executable-Commands/Core-Commands/User-Utilities/Copy-and-Convert/dd.h
+++ Operating-System/Executable-Commands/Core-Commands/User-Utilities/Copy-and-Convert/dd.h
@@ -1,73 +1,34 @@
-/*-
- * Copyright (c) 1991, 1993, 1994
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Keith Muller of the University of California, San Diego and Lance
- * Visser of Convex Computer Corporation.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)dd.h	8.3 (Berkeley) 4/2/94
- * $FreeBSD$
- */
-
-/* Input/output stream state. */
 typedef struct {
-	u_char		*db;		/* buffer address */
-	u_char		*dbp;		/* current buffer I/O address */
-	/* XXX ssize_t? */
-	size_t		dbcnt;		/* current buffer byte count */
-	size_t		dbrcnt;		/* last read byte count */
-	size_t		dbsz;		/* buffer size */
+	u_char		*db;
+	u_char		*dbp;
+	size_t		dbcnt;
+	size_t		dbrcnt;
+	size_t		dbsz;
 
-#define	ISCHR		0x01		/* character device (warn on short) */
-#define	ISPIPE		0x02		/* pipe-like (see position.c) */
-#define	ISTAPE		0x04		/* tape */
-#define	ISSEEK		0x08		/* valid to seek on */
-#define	NOREAD		0x10		/* not readable */
-#define	ISTRUNC		0x20		/* valid to ftruncate() */
+#define	ISCHR		0x01
+#define	ISPIPE		0x02
+#define	ISTAPE		0x04
+#define	ISSEEK		0x08
+#define	NOREAD		0x10
+#define	ISTRUNC		0x20
+
 	u_int		flags;
-
-	const char	*name;		/* name */
-	int		fd;		/* file descriptor */
-	off_t		offset;		/* # of blocks to skip */
+	const char	*name;
+	int		fd;
+	off_t		offset;
 } IO;
 
 typedef struct {
-	uintmax_t	in_full;	/* # of full input blocks */
-	uintmax_t	in_part;	/* # of partial input blocks */
-	uintmax_t	out_full;	/* # of full output blocks */
-	uintmax_t	out_part;	/* # of partial output blocks */
-	uintmax_t	trunc;		/* # of truncated records */
-	uintmax_t	swab;		/* # of odd-length swab blocks */
-	uintmax_t	bytes;		/* # of bytes written */
-	double		start;		/* start time of dd */
+	uintmax_t	in_full;
+	uintmax_t	in_part;
+	uintmax_t	out_full;
+	uintmax_t	out_part;
+	uintmax_t	trunc;
+	uintmax_t	swab;
+	uintmax_t	bytes;
+	double		start;
 } STAT;
 
-/* Flags (in ddflags). */
 #define	C_ASCII		0x00001
 #define	C_BLOCK		0x00002
 #define	C_BS		0x00004
@@ -95,5 +56,4 @@ typedef struct {
 #define	C_UCASE		0x10000000
 #define	C_UNBLOCK	0x20000000
 #define	C_FILL		0x40000000
-
 #define	C_PARITY	(C_PAREVEN | C_PARODD | C_PARNONE | C_PARSET)
